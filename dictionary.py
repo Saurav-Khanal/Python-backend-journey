8c0170a6db22ea56d421311ba7016358db21840c
Sauravc-Dev <SAURAVKHANAL842@GMAIL.COM>
2026-08-14 14:06:32 +0545

    Practiced Python dictionaries

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+book={
+    "title":"Atomic Habits",
+    "author":"James clear",
+    "price":800
+}
+book["author"]="saurav"
+book["price"]=1200
+book["title"]="Saurav Journey"
+book["year"]=2025
+print(book["title"])
+print(book["author"])
+print(book["price"])
+print(book["year"])
